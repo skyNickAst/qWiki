d47ae6572df90608bea12ba0587a2bf732c6c7be
Nikolai Astakhov <skynetprojectone@gmail.com>
2023-01-16 19:51:33 +0300

    Initial Commit

---
+++ qWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qWiki
+//
+//  Created by Nikolai Astakhov on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
